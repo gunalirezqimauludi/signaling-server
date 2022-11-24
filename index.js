f28d20ac057259680ded870aa45b8cabbe7b7ccf
Gunali Rezqi Mauludi <hello.gunalirezqi@gmail.com>
2022-11-24 19:09:47 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
+const app = express()
+const port = process.env.PORT || 8000;
+const server = http.createServer(app)
+const io = socketIO(server, {
+    cors: {
+        origin : '*'
+    }
+})
+
+const offerMap = new Map();  
+
+io.on('connection', (socket) => {
+    console.log("socket connected: ", socket.id);
+
+    socket.on('join', ({roomId}) => {
+        socket.join(roomId);
+        console.log("join: ", roomId);
+
+        const prevOffer = offerMap.get(roomId);
+        socket.emit('remote-offer', {offer: prevOffer});
+        console.log("remote-offer: ", {offer: prevOffer});
+    })
+    
+    socket.on('new-offer', ({offer, roomId}) => {
+        offerMap.set(roomId, offer);
+        console.log("new-offer: ", offer);
+    })
+
+    socket.on('new-answer', ({answer, roomId}) => {
+        socket.to(roomId).emit('remote-answer', {answer})
+        console.log("remote-answer: ", {answer});
+    })
+
+    socket.on('new-ice', ({iceCandidates, roomId}) => {
+        socket.to(roomId).emit('remote-ice', {iceCandidates})
+        console.log("remote-ice: ", {iceCandidates});
+    })
+
+})
+
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
